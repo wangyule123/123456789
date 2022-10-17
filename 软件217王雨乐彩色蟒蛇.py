a95f1d6b2db530dd4369ae3274c1447803832b25
wangyule123 <115970071+wangyule123@users.noreply.github.com>
2022-10-17 13:25:30 +0800

    b

---
+++ 软件217王雨乐彩色蟒蛇.py
@@ -0,0 +1,17 @@
+import turtle as t
+from time import*
+t.setup(1000, 1000)
+t.penup()
+t.fd(-200)
+t.pendown()
+colors = ['red', 'pink', 'green', 'blue']
+t.pensize(20)
+for i, color in zip(range(4), colors):
+    t.pencolor(color)
+    t.circle(-40, 80)
+    t.circle(40, 80)
+t.circle(40, 40)
+t.fd(30)
+t.circle(16, 100)
+t.fd(30)
+sleep(10)
